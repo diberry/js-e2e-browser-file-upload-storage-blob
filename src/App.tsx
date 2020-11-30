df7f7544ed911332f874be9437b1ed2a839c956e
diberry <diberry@microsoft.com>
2020-11-30 10:51:11 -0800

    final sso with file upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,17 @@ import {
   deleteBlob,
   deleteContainer,
   getContainerNameWithUserName,
-  setBlobMetadataProperties
+  getContainerProperties,
 } from './azure/azure-storage-blob';
 import AzureAuthenticationButton from './azure/azure-authentication-component';
 import { AccountInfo } from '@azure/msal-browser';
-import { computerVision, isConfigured as isComputerVisionConfigured } from './azure/azure-cognitiveservices-computervision';
 
 const App = (): JSX.Element => {
   // all blobs in container
   const [blobList, setBlobList] = useState<AppBlobInfo[]>([]);
+
+  // container properties
+  const [containerProperties, setContainerProperties] = useState({});
 
   // current file to upload into container
   const [fileSelected, setFileSelected] = useState<File | null>();
@@ -23,23 +25,20 @@ const App = (): JSX.Element => {
   // current authenticated user
   const [currentUser, setCurrentUser] = useState<AccountInfo>();
 
+  // current user's container name
+  const [userContainerName, setUserContainerName] = useState<string>('');
+
   // UI/form management
   const [uploading, setUploading] = useState<Boolean>(false);
 
-  // Cognitive Services ComputerVision 
-  const isCognitiveServicesComputerVisionConfigured = isComputerVisionConfigured();
-  
   const onFileChange = (event: any) => {
     // capture file into state
     setFileSelected(event.target.files[0]);
-    
-    
   };
 
   const onFileDelete = async (filename: any) => {
-    
     if (!currentUser) throw Error("user isn't valid");
-    
+
     // *** DELETE BLOB IN AZURE STORAGE ***
     await deleteBlob(currentUser, filename);
 
@@ -48,9 +47,8 @@ const App = (): JSX.Element => {
     setBlobList(filesFromContainer);
   };
   const onContainerDelete = async () => {
-    
     if (!currentUser) throw Error("user isn't valid");
-    
+
     setBlobList([]);
 
     // *** DELETE CONTAINER IN AZURE STORAGE ***
@@ -58,25 +56,13 @@ const App = (): JSX.Element => {
   };
 
   const onFileUpload = async () => {
-    
     if (fileSelected && currentUser) {
-      
       // prepare UI
       setUploading(true);
 
       // *** AZURE - UPLOAD TO AZURE STORAGE ***
-      const createBlobResults = await createBlobInContainer(fileSelected, currentUser);
+      await createBlobInContainer(fileSelected, currentUser);
 
-      let computerVisionPrediction = null;
-      
-      // *** AZURE - COGNITIVE SERVICES COMPUTER VISION ***
-      if (isCognitiveServicesComputerVisionConfigured && fileSelected && createBlobResults.URL.length > 0) {
-        computerVisionPrediction = await computerVision(createBlobResults.URL) ;
-      } 
-      
-      // *** AZURE - UPDATE FILE METADATA ***
-      if (computerVisionPrediction) await setBlobMetadataProperties(fileSelected, currentUser, computerVisionPrediction);
-      
       // *** AZURE - GET BLOBS FROM AZURE STORAGE ***
       const filesFromContainer = await getBlobsInContainer(currentUser);
       setBlobList(filesFromContainer);
@@ -89,6 +75,15 @@ const App = (): JSX.Element => {
 
   const onAuthenticated = async (userAccountInfo: AccountInfo) => {
     setCurrentUser(userAccountInfo);
+
+    const containerName = getContainerNameWithUserName(userAccountInfo);
+    setUserContainerName(containerName);
+
+    // *** AZURE - GET CONTAINER PROPERTIES FROM AZURE STORAGE ***
+    const containerProperties = containerName && userAccountInfo ? await getContainerProperties(userAccountInfo) : {};
+    setContainerProperties(containerProperties);
+
+    // *** AZURE - GET BLOBS FROM AZURE STORAGE ***
     const blobsInContainer: AppBlobInfo[] = await getBlobsInContainer(userAccountInfo);
     setBlobList(blobsInContainer);
   };
@@ -107,8 +102,14 @@ const App = (): JSX.Element => {
 
   // render upload form
   const DisplayForm = ({ user }: any) => {
+    const divStyle = {
+      leftMargin: '5px',
+      color: 'black',
+      backgroundColor: '#FFFF99',
+    };
+
     return (
-      <div id="DisplayForm">
+      <div id="DisplayForm" style={divStyle}>
         <hr />
         <h4>Select an image</h4>
         {!fileSelected ? (
@@ -124,7 +125,7 @@ const App = (): JSX.Element => {
           <div>
             {!uploading ? (
               <button type="submit" onClick={onFileUpload}>
-                Upload
+                Upload File
               </button>
             ) : (
               <div>Uploading...</div>
@@ -134,31 +135,42 @@ const App = (): JSX.Element => {
             </div>
           </div>
         )}
+        <br />
       </div>
     );
   };
 
-  // render user's images
+  // render user's container images
   const DisplayImagesFromContainer = ({ user }: any) => {
+    const expandedContainerProperties = { name: userContainerName, ...containerProperties, count: blobList.length };
+
     return (
       <div>
-        <hr />
-        <h4>
-          {getContainerNameWithUserName(currentUser)} images: ({blobList.length}){' '}
-          <button onClick={() => onContainerDelete()}>X</button>
-        </h4>
+        {currentUser && (
+          <div>
+            <hr />
+            <PrettyPrintJson name={userContainerName} data={expandedContainerProperties} />
+            <button onClick={() => onContainerDelete()}>Delete container</button>
+          </div>
+        )}
         <ol>
-          {blobList.map((item, index) => {
-            return (
-              <li key={index} data-id={item.name}>
-                <div>
-                  <PrettyPrintJson name={item.name} data={item} />
-                  <img src={item.url} alt={item.name} height="200" />
-                  <button onClick={() => onFileDelete(item.name)}>X</button>
-                </div>
-              </li>
-            );
-          })}
+          {blobList.length > 0 &&
+            blobList.map((item, index) => {
+              return (
+                <li key={index} data-id={item.name}>
+                  <div>
+                    <PrettyPrintJson name={item.name} data={item} />
+                    <a href={item.url}>
+                      <img src={item.url} alt={item.name} height="200" />
+                    </a>
+                    <div>
+                      <button onClick={() => onFileDelete(item.name)}>Delete {item.name}</button>
+                    </div>
+                    <br />
+                  </div>
+                </li>
+              );
+            })}
         </ol>
       </div>
     );
@@ -173,7 +185,7 @@ const App = (): JSX.Element => {
         ) : (
           <div>
             <DisplayForm user={user} />
-            {blobList.length > 0 && <DisplayImagesFromContainer user={user} />}
+            <DisplayImagesFromContainer user={user} />
           </div>
         )}
       </div>
--- src/azure/azure-storage-blob.ts
+++ src/azure/azure-storage-blob.ts
@@ -2,43 +2,51 @@ import { BlobServiceClient } from '@azure/storage-blob';
 import { AccountInfo } from '@azure/msal-browser';
 import Sanitize from 'sanitize-filename';
 
-const containerName = process.env.REACT_APP_AZURE_STORAGE_CONTAINER_PREFIX;
+const containerNamePrefix = process.env.REACT_APP_AZURE_STORAGE_CONTAINER_PREFIX;
 const sasToken = process.env.REACT_APP_AZURE_STORAGE_SAS_TOKEN;
 const storageAccountName = process.env.REACT_APP_AZURE_STORAGE_ACCOUNT_NAME;
+
+const userContainer: any = {
+  name: '',
+  client: null,
+};
 
 export const isStorageConfigured = () => {
   return !storageAccountName || !sasToken ? false : true;
 };
 
 export const getContainerNameWithUserName = (userAccount: AccountInfo | undefined) => {
-  
   if (!userAccount || userAccount?.username.length === 0) throw Error('Azure userAccount.username is invalid');
+
+  if (userContainer && userContainer.name !== '') return userContainer.name;
 
   const sanitizedName = Sanitize(userAccount?.username);
   const removeEmailChars = sanitizedName?.replace(/[@.]/gi, '-');
-  const userContainerName = `${containerName}-${removeEmailChars}`;
+  userContainer.name = `${containerNamePrefix}-${removeEmailChars}`.toLowerCase();
 
-  return userContainerName.toLowerCase();
+  return userContainer.name;
 };
 
 const getContainerClient = async (userAccount: AccountInfo | undefined): Promise<any> => {
-  
   if (!userAccount || userAccount?.username.length === 0) throw Error('userAccount is invalid');
   if (!storageAccountName || storageAccountName.length === 0) throw Error('storageAccountName is invalid');
   if (!sasToken || sasToken.length === 0) throw Error('sasToken is invalid');
 
+  if (userContainer && userContainer.name !== '' && userContainer.client != null) return userContainer;
+
   const blobService = new BlobServiceClient(`https://${storageAccountName}.blob.core.windows.net/?${sasToken}`);
 
   // create container name: username + env var container prefix
-  const containerName = getContainerNameWithUserName(userAccount);
+  userContainer.name = getContainerNameWithUserName(userAccount);
 
   // get Container - full public read access
-  const containerClient = blobService.getContainerClient(containerName);
+  userContainer.client = blobService.getContainerClient(userContainer.name);
 
   // return client
-  const container = await containerClient.createIfNotExists({ access: 'container' });
+  // `blob` access means you can share blob publicly but not container listing of blobs
+  await userContainer.client.createIfNotExists({ access: 'blob' });
 
-  return { container, containerClient };
+  return userContainer;
 };
 
 // reduce data set to manageable info
@@ -53,7 +61,6 @@ export type AppBlobInfo = {
   url: string;
 };
 
-
 // return list of blobs in container to display
 export const getBlobsInContainer = async (userAccount: AccountInfo | undefined): Promise<AppBlobInfo[]> => {
   if (!userAccount || userAccount?.username.length === 0) throw Error('userAccount is invalid');
@@ -65,7 +72,9 @@ export const getBlobsInContainer = async (userAccount: AccountInfo | undefined):
 
   // get list of blobs in container
   // eslint-disable-next-line
-  for await (const blob of container.containerClient.listBlobsFlat()) {
+  for await (const blob of container.client.listBlobsFlat({
+    includeMetadata: true,
+  })) {
     const simplifiedBlob: AppBlobInfo = {
       name: blob.name,
       createdOn: blob.properties.createdOn,
@@ -74,9 +83,7 @@ export const getBlobsInContainer = async (userAccount: AccountInfo | undefined):
       contentLength: blob.properties.contentLength,
       contentType: blob.properties.contentType,
       blobType: blob.properties.blobType,
-      url: `https://${storageAccountName}.blob.core.windows.net/${getContainerNameWithUserName(userAccount)}/${
-        blob.name
-      }`,
+      url: blob.metadata['url'],
     };
 
     returnedBlobUrls.push(simplifiedBlob);
@@ -86,29 +93,38 @@ export const getBlobsInContainer = async (userAccount: AccountInfo | undefined):
 };
 
 export const createBlobInContainer = async (file: File, userAccount: AccountInfo) => {
-  
   if (!userAccount || userAccount?.username.length === 0) throw Error('userAccount is invalid');
   if (!file) throw Error('file is invalid');
 
   const container = await getContainerClient(userAccount);
 
-  // create blobClient for container
-  const blobClient = container.containerClient.getBlockBlobClient(file.name.toLowerCase());
+  // create blobClient for container, includes URL
+  const blobClient = container.client.getBlockBlobClient(file.name.toLowerCase());
 
   // set mimetype as determined from browser with file upload control
   const options = { blobHTTPHeaders: { blobContentType: file.type } };
 
   // upload file
   const uploadBrowserData = await blobClient.uploadBrowserData(file, options);
-  
-  return { ...uploadBrowserData,
-    URL: `https://${storageAccountName}.blob.core.windows.net/${getContainerNameWithUserName(userAccount)}/${file.name
-      }`, 
-  };
+
+  console.log(`${blobClient.url}`);
+
+  // add url to metadata
+  await setBlobMetadataProperties(file, userAccount, { url: blobClient.url });
+
+  return { ...uploadBrowserData, url: blobClient.url };
+};
+
+export const getContainerProperties = async (userAccount: AccountInfo) => {
+  if (!userAccount || userAccount?.username.length === 0) throw Error('userAccount is invalid');
+
+  const container = await getContainerClient(userAccount);
+
+  // get properties
+  return await container.client.getProperties();
 };
 
 export const setBlobMetadataProperties = async (file: File, userAccount: AccountInfo, metaData: any) => {
-  
   if (!userAccount || userAccount?.username.length === 0) throw Error('userAccount is invalid');
   if (!file) throw Error('file is invalid');
   if (!metaData) throw Error('metaData is invalid');
@@ -116,37 +132,35 @@ export const setBlobMetadataProperties = async (file: File, userAccount: Account
   const container = await getContainerClient(userAccount);
 
   // create blobClient for container
-  const blobClient = container.containerClient.getBlockBlobClient(file.name.toLowerCase());
+  const blobClient = container.client.getBlockBlobClient(file.name.toLowerCase());
 
   // upload file
   const setMetadataResults = await blobClient.setMetadata(metaData);
-  
+
   return setMetadataResults;
 };
 
 export const deleteContainer = async (userAccount: AccountInfo): Promise<any> => {
-  
   if (!userAccount || userAccount?.username.length === 0) throw Error('userAccount is invalid');
 
   // get Container - full public read access
   const container = await getContainerClient(userAccount);
 
   // time dependent operation - all subsequent operations should poll for completeness
-  const deleteResults = await container.containerClient.deleteIfExists();
+  const deleteResults = await container.client.deleteIfExists();
 
   return deleteResults;
 };
 
 export const deleteBlob = async (userAccount: AccountInfo, fileName: string): Promise<any> => {
-
   if (!userAccount || userAccount?.username.length === 0) throw Error('userAccount is invalid');
-    if (!fileName) throw Error('fileName is invalid');
+  if (!fileName) throw Error('fileName is invalid');
 
   // get Container - full public read access
   const container = await getContainerClient(userAccount);
 
   // get blob client
-  const blockBlobClient = container.containerClient.getBlockBlobClient(fileName);
+  const blockBlobClient = container.client.getBlockBlobClient(fileName);
 
   // @ts-ignore
   const deleteResults = await blockBlobClient.deleteIfExists();
